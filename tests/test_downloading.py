b1fc3ff4ad3396caaf1d547d60323239097db929
Peter Rupnik <peter.rupnik@ijs.si>
2026-02-06 12:47:20 +0100

    Add demo, add handlers for metadata inclusion testing

--- tests/test_downloading.py
+++ tests/test_downloading.py
@@ -8,8 +8,9 @@ import polars as pl
 url = "https://parlacap.ipipan.waw.pl/"
 
 
+@pytest.mark.parametrize("metadata", [True, False])
 @pytest.mark.parametrize("format", formats)
-def test_downloading_file(format):
+def test_downloading_file(format: str, metadata: bool):
     filter = {
         "filter": {"column": "parliament", "value": "SI", "operator": "="},
         "limit": 10,
@@ -18,31 +19,34 @@ def test_downloading_file(format):
     }
 
     response = requests.post(
-        url + f"download?format={format}",
+        url + f"download?format={format}&include_metadata={str(metadata).lower()}",
         json=filter,
     )
     if not response.status_code == 200:
         raise Exception(
             f"Got weird response code: {response.status_code}, response text: {response.text}"
         )
-    p = Path(f"downloaded_data.{format}")
+    p = Path(f"downloaded_data{'_metadata' if metadata else '_no_metadata'}.{format}")
     p.write_bytes(response.content)
     assert p.exists()
 
     handler = {
-        "csv": lambda s: pl.read_csv(
+        ("csv", False): lambda s: pl.read_csv(
             s,
-            #  skip_rows=7
         ),
-        "tsv": lambda s: pl.read_csv(
+        ("csv", True): lambda s: pl.read_csv(s, skip_rows=7),
+        ("tsv", False): lambda s: pl.read_csv(
             s,
             separator="\t",
-            #  skip_rows=7
         ),
-        "parquet": pl.read_parquet,
+        ("tsv", True): lambda s: pl.read_csv(s, separator="\t", skip_rows=7),
+        ("parquet", False): pl.read_parquet,
+        ("parquet", True): pl.read_parquet,
+        ("xlsx", True): pl.read_excel,
+        ("xlsx", False): pl.read_excel,
     }
-    if handler.get(format, False):
-        df = handler[format](p)
+    if handler.get((format, metadata), False):
+        df = handler[(format, metadata)](p)
         assert df.height == 10
 
     # p.unlink()
